9a831745910f8b6d2137b7185f5e9e396a42b9f3
Vahan Mkrtumyan <vahanmkrtumyan@gmail.com>
2019-01-09 15:48:48 +0400

    Update table.jsx

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -4,7 +4,7 @@ import TableBody from "./tableBody";
 
 const Table = ({ columns, sortColumn, onSort, data }) => {
   return (
-    <table className="table">
+    <table className="table table-hover">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
       <TableBody columns={columns} data={data} />
     </table>
